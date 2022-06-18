102d014688bb8f33114f0b4a5b76033ca6ff4a59
17101448 <the_1_week@naver.com>
2022-06-19 01:20:12 +0900

    본문 내용까지 받아오기

--- news_summary/source/crawling.py
+++ news_summary/source/crawling.py
@@ -3,11 +3,10 @@ from bs4  import BeautifulSoup
 import bs4.element
 import datetime
 
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
 
 def rank_news_crawling():
     base_url = "https://news.naver.com/main/ranking/popularDay.naver?mid=etc&sid1=111" \
-
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
     # web page information 
     res = requests.get(base_url, headers = headers)
     # parsing 
@@ -20,13 +19,27 @@ def rank_news_crawling():
             "title" : li.a.string,
             "links" : li.a["href"],
             "upload_time" : li.span.string,
-            "img_src" : li.img["src"]
+            "img_src" : li.img["src"],
+            "content" : article(li.a["href"])
 
         }
         news_list.append(news_info)
 
 
     return news_list 
+
+def article(link):
+    data = ""
+    res = requests.get(link, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    contents = soup.find("div", id = "dic_area")
+    for content in contents:
+        if type(content) is bs4.element.NavigableString:
+            
+            data += content
+            data += " "
+    
+    return data
 
 def save_list(list):
     with open('test.txt', 'w', encoding='UTF-8') as f:
